7f0f05e40ece59211df0ad9f75c59ffc8a2b3e7f
User <user@Users-MacBook-Pro.local>
2020-01-28 09:48:49 +0000

    waterbottle test error,added soloution,lost my file

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,21 +1,23 @@
 public class WaterBottle {
-
-public int volume;
-
+    private int volume;
 
     public WaterBottle() {
         this.volume = 100;
     }
-    public int drink() {
-        return this.volume -= 10;
+
+    public void drink(){
+        this.volume -= 10;
     }
 
-    public int empty() {
-        return this.volume = 0;
+    public void empty(){
+        this.volume = 0;
     }
 
-    public int fill() {
-        return this.volume = 100;
+    public void fill(){
+        this.volume = 100;
     }
 
+    public int getVolume() {
+        return volume;
+    }
 }
